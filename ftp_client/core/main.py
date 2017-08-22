1e497671e24d64380d57811329ac056742bdd1c7
pefami <pefami163@gmail.com>
2017-08-22 15:55:00 +0800

    完成ftp的目录切换功能

--- ftp_client/core/main.py
+++ ftp_client/core/main.py
@@ -1,60 +1,106 @@
 #! /usr/bin/env python3
 # -*-encoding:utf-8 -*-
-#@Time 2017/8/21 15:56
-#@Author pefami
-import socket,json
+# @Time 2017/8/21 15:56
+# @Author pefami
+import socket, json
 
 from ftp_client.conf import Settings
 
 
 class FtpClient:
-    ServerAddress=(Settings.ServerIp,Settings.ServerPort)
+    ServerAddress = (Settings.ServerIp, Settings.ServerPort)
+
     def __init__(self):
-        commands = {
+        self.local_commands = {
             "h": self._help,
-            "help":self._help
+            "help": self._help,
+            "ls": self._command_ls,
+            "cd": self._command_cd
         }
-        self.client=socket.socket()
+        self.client = socket.socket()
 
     def connet_serve(self):
         self.client.connect(FtpClient.ServerAddress)
-        #连接以后进行用户认证
+        # 连接以后进行用户认证
         self.auth_identity()
-        #连接成功后进行操作
+        # 连接成功后进行操作
         self.local_shell()
 
     def local_shell(self):
-        while True :
-            #进行命令操作
-            command=input(self.__current_path+" >>>")
-
+        while True:
+            # 进行命令操作
+            command = input("\n" + self.__current_path + " >>:")
+            items = command.split(" ")
+            # 去掉多余的空格
+            items = list(filter(lambda x: x, items))
+            if len(items) > 0 and items[0] in self.local_commands:
+                self.local_commands[items[0]](items)
+            else:
+                print("无效的命令")
 
     def auth_identity(self):
-        while True :
-            username=input("Username:")
-            password=input("Password:")
-            #发送给服务端进行验证
-            auth_data=json.dumps({"username":username,"password":password})
+        while True:
+            username = input("Username:")
+            password = input("Password:")
+            # 发送给服务端进行验证
+            auth_data = json.dumps({"username": username, "password": password})
             self.client.sendall(auth_data.encode("utf-8"))
 
-            #等待服务端结果返回
-            auth_result=self.client.recv(1024)
-            auth_result=auth_result.decode("utf-8")
-            if auth_result == "success" :
-                #认证成功
+            # 等待服务端结果返回
+            auth_result = self.client.recv(1024)
+            auth_result = auth_result.decode("utf-8")
+            if auth_result == "success":
+                # 认证成功
                 print("认证成功")
-                self.__current_path="home"
+                # 查询一次当前路径
+                self._command_cd("cd".split(" "))
                 break
-            else :
-                #认证失败，显示失败信息
+            else:
+                # 认证失败，显示失败信息
                 print(auth_result)
 
-    def _help(self):
-        #显示帮助信息
+    def _help(self, *args):
+        # 显示帮助信息
+        desc = {
+            "h,help": "显示帮助信息",
+            "ls ‘path’": "显示指定路径目录信息，无参时显示当前目录",
+            "cd 'path'": "切换到指定路径，无参时切换到当前路径"
+        }
+        for key, value in desc.items():
+            print(key, value, end="   \n")
+
+    def _command_ls(self):
         pass
 
+    def _command_cd(self, *args):
+        # 切换到指定目录
+        command = args[0]
+        result = self._sendCommand(" ".join(command))
+        result=result.decode("utf-8")
+        result_dict=json.loads(result)
+        if "path" in result_dict :
+            self.__current_path = result_dict["path"]
+        elif "msg" in result_dict:
+            print(result_dict["msg"])
+
+    def _sendCommand(self, command):
+        # 远程的命令，发送给服务端处理
+        self.client.sendall(command.encode("utf-8"))
+        result_len = int(self.client.recv(1024).decode("utf-8"))
+        recv_len = 0
+        result_data = bytes()
+        # 准备就绪，通知后台发送结果
+        # print(result_len,recv_len)
+        self.client.sendall("ok".encode("utf-8"))
+        while recv_len != result_len:
+            data = self.client.recv(1024)
+            recv_len += len(data)
+            result_data += data
+        return result_data
+
+
 def run():
-    #创建ftp客户端
-    ftp=FtpClient()
-    #连接服务端
+    # 创建ftp客户端
+    ftp = FtpClient()
+    # 连接服务端
     ftp.connet_serve()
--- ftp_serve/core/serve.py
+++ ftp_serve/core/serve.py
@@ -10,9 +10,13 @@ from ftp_serve.conf import Settings
 # 处理客户端连接请求
 class PFMRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        self.__user = None
-        self.auth_identity()
-        # 认证成功，切换到该用户当前目录下
+        try:
+            self.__user = None
+            self.auth_identity()
+            # 认证成功，切换到该用户当前目录下
+            self.handle_command()
+        except Exception as e:
+            print(e)
 
     def auth_identity(self):
         while True:
@@ -24,17 +28,18 @@ class PFMRequestHandler(socketserver.BaseRequestHandler):
             # 根据username去数据库中查找用户是否存在
             conf_path = os.path.dirname(os.path.abspath(__file__))
             serve_base_path = os.path.dirname(conf_path)
-            user_path = os.path.join(serve_base_path, "db", username)
-            if os.path.isdir(user_path):
+            self.__user_path = os.path.join(serve_base_path, "db", username)
+            if os.path.isdir(self.__user_path):
                 # 用户存在,验证用户的密码是否正确
-                user_info = os.path.join(user_path, "config")
+                user_info = os.path.join(self.__user_path, "config")
                 if os.path.isfile(user_info):
                     with open(user_info, "r", encoding="utf-8") as f:
                         info = json.load(f)
                         if password == info["password"]:
                             # 密码正确，登录成功
                             self.__user = username
-                            self.__current_path = os.path.join(user_path, "home")
+                            self.__show_path = "home"
+                            self.__current_path = os.path.join(self.__user_path, self.__show_path)
                             self.request.sendall("success".encode("utf-8"))
                             break
                         else:
@@ -46,6 +51,83 @@ class PFMRequestHandler(socketserver.BaseRequestHandler):
             else:
                 # 用户不存在
                 self.request.sendall("该用户不存在！".encode("utf-8"))
+
+    def handle_command(self):
+        self.commands = {
+            "ls": self._command_ls,
+            "cd": self._command_cd
+        }
+        while True:
+            data = self.request.recv(1024)
+            command = data.decode("utf-8")
+            items = command.split(" ")
+            # 去掉多余的空格
+            items = list(filter(lambda x: x, items))
+            if len(items) > 0 and items[0] in self.commands:
+                self.commands[items[0]](items)
+            else:
+                self._send_result("无效的命令")
+
+    def _send_result(self, data):
+        if isinstance(data, str):
+            # 返回的为字符串
+            data = data.encode("utf-8")
+        data_len = len(data)
+        self.request.sendall(str(data_len).encode("utf-8"))
+
+        is_send = self.request.recv(1024).decode("utf-8")
+        if is_send != "ok": return
+        self.request.sendall(data)
+
+    def _command_ls(self, *args):
+        pass
+
+    # C:\Users\Administrator\PycharmProjects\ftp_simple\ftp_serve\db\root
+    def _command_cd(self, *args):
+        command = args[0]
+        if len(command) == 1:
+            # 没有指定路径
+            self._send_result(json.dumps({"path": self.__show_path}))
+        else:
+            path = command[1]
+            # 判断路径是否是绝对路径
+            if os.path.isabs(path):
+                # 绝对路径
+                if path.startswith(self.__user_path, 0, len(path) - 2):
+                    last_path = path[len(self.__user_path) + 1:]
+                    # 判断路径是否存在
+                    if os.path.isdir(path):
+                        # 存在，切换到指定目录下
+                        self.__show_path = last_path
+                        self.__current_path = path
+                        self._send_result(json.dumps({"path": self.__show_path}))
+                    else:
+                        # 不存在，返回错误信息
+                        self._send_result(json.dumps({"msg": "访问目录不存在"}))
+                else:
+                    # 路径属于绝对路径，但不是在该用户目录下，无权访问
+                    self._send_result(json.dumps({"msg": "无权访问该目录"}))
+            else:
+                # 相对路径，拼接全路径
+                if path == "..":
+                    # 返回上一个路径
+                    abs_path = os.path.dirname(self.__current_path)
+                    if abs_path == self.__user_path:
+                        self._send_result(json.dumps({"msg": "已经到了能访问的最上层了"}))
+                        return
+                elif path=="." or path.startswith(".") :
+                    #以点开头，表示当前目录
+                    self._send_result(json.dumps({"path": self.__show_path}))
+                    return
+                else:
+                    abs_path = os.path.join(self.__current_path, path)
+
+                if os.path.isdir(abs_path):
+                    self.__show_path = abs_path[len(self.__user_path) + 1:]
+                    self.__current_path=abs_path
+                    self._send_result(json.dumps({"path": self.__show_path}))
+                else:
+                    self._send_result(json.dumps({"msg": "访问目录不存在"}))
 
 
 # 开启FTP服务端
